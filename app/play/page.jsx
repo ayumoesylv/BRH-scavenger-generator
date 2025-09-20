ff054470efe15daa52ae7ccb2f5f5f6dd4b07666
sylviazhang <sylvia.zhang88@outlook.com>
2025-09-20 14:42:16 -0400

    timer

--- app/play/page.jsx
+++ app/play/page.jsx
@@ -1,7 +1,70 @@
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+import { useRouter } from "next/navigation";
+//import final list of clues from edit/page.jsx 
+
+function Stopwatch() {
+  const [isRunning, setIsRunning] = useState(false);
+  const[elapsedTime, setElapsedTime] = useState(0);
+  const intervalIdRef = useRef(null);
+  const startTimeRef = useRef(0);
+
+  useEffect(() => {
+
+    if(isRunning){
+      intervalIdRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current)
+      }, 10);
+    }
+
+    return() => {
+      clearInterval(intervalIdRef.current);
+
+    }
+
+  }, [isRunning]);
+
+  function start(){
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime
+
+  }
+  function stop(){
+    setIsRunning(false)
+    
+  }
+  function formatTime(){
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / (1000) % 60);
+    let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+    hours = String(hours).padStart(2, "0")
+    minutes = String(minutes).padStart(2, "0")
+    seconds = String(seconds).padStart(2, "0")
+
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
+  return(
+    <div className='stopwatch'>
+      <div className='display'>{formatTime()}</div>
+      <div className='controls'>
+        <button onClick={start} className='start-button'>Start</button>
+        <button onClick={stop} className='stop-button'>Stop</button>
+      </div>
+    </div>
+  );
+
+}
+
 export default function PlayPage() {
   return (
     <main className="min-h-screen flex items-center justify-center p-6">
-      <h1 className="text-2xl font-semibold">timer and final cllues here</h1>
+      <section>
+        <Stopwatch />
+      </section>
     </main>
   );
 }
